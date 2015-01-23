eacf2c67d6f037a49b029a3ae103d87b8bcfd345
smallow <playdreamist@gmail.com>
2015-01-23 11:45:15 +0800

    添加refresh下拉刷新控件

--- app/src/main/java/com/smallow/android/news/fragments/CategoryFragment.java
+++ app/src/main/java/com/smallow/android/news/fragments/CategoryFragment.java
@@ -1,16 +1,27 @@
 package com.smallow.android.news.fragments;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.handmark.pulltorefresh.library.ILoadingLayout;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.smallow.android.news.R;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by smallow on 2015/1/22.
@@ -19,6 +30,10 @@ public class CategoryFragment extends BaseFragment {
 
     private String title;
     private String categoryCode;
+    private PullToRefreshListView mPullRefreshListView;
+    private ArrayAdapter<String> mAdapter;
+    private LinkedList<String> mListItems;
+    private int mItemCount = 9;
 
     public CategoryFragment() {
 
@@ -36,6 +51,94 @@ public class CategoryFragment extends BaseFragment {
 
     @Override
     protected void onInitWidgets(View rootView, Bundle savedInstanceState) {
-        ((TextView) findViewById(R.id.category_name)).setText(title + " : " + categoryCode);
+
+        mPullRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_refresh_list);
+        //初始化数据
+        initDatas();
+        initIndicator();
+        mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mListItems);
+        mPullRefreshListView.setAdapter(mAdapter);
+        /*mPullRefreshListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+                String label = DateUtils.formatDateTime(
+                        getActivity().getApplicationContext(),
+                        System.currentTimeMillis(),
+                        DateUtils.FORMAT_SHOW_TIME
+                                | DateUtils.FORMAT_SHOW_DATE
+                                | DateUtils.FORMAT_ABBREV_ALL);
+                // 显示最后更新的时间
+                refreshView.getLoadingLayoutProxy()
+                        .setLastUpdatedLabel(label);
+                // 模拟加载任务
+                new GetDataTask().execute();
+            }
+        });*/
+        mPullRefreshListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ListView>() {
+            @Override
+            public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+                //这里写下拉刷新的任务
+                String label = DateUtils.formatDateTime(
+                        getActivity().getApplicationContext(),
+                        System.currentTimeMillis(),
+                        DateUtils.FORMAT_SHOW_TIME
+                                | DateUtils.FORMAT_SHOW_DATE
+                                | DateUtils.FORMAT_ABBREV_ALL);
+                // 显示最后更新的时间
+                refreshView.getLoadingLayoutProxy()
+                        .setLastUpdatedLabel(label);
+                new GetDataTask().execute();
+            }
+
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+                //这里写上拉加载更多的任务
+                new GetDataTask().execute();
+            }
+        });
+
+    }
+
+    private void initDatas() {
+// 初始化数据和数据源
+        mListItems = new LinkedList<String>();
+
+        for (int i = 0; i < mItemCount; i++) {
+            mListItems.add("" + i);
+        }
+    }
+    private void initIndicator()
+    {
+        ILoadingLayout startLabels = mPullRefreshListView
+                .getLoadingLayoutProxy(true, false);
+        startLabels.setPullLabel("你可劲拉，拉...");// 刚下拉时，显示的提示
+        startLabels.setRefreshingLabel("好嘞，正在刷新...");// 刷新时
+        startLabels.setReleaseLabel("你敢放，我就敢刷新...");// 下来达到一定距离时，显示的提示
+
+        ILoadingLayout endLabels = mPullRefreshListView.getLoadingLayoutProxy(
+                false, true);
+        endLabels.setPullLabel("加载更多数据...");// 刚下拉时，显示的提示
+        endLabels.setRefreshingLabel("好嘞，正在加载数据...");// 刷新时
+        endLabels.setReleaseLabel("你敢放，我就敢完成加载...");// 下来达到一定距离时，显示的提示
+    }
+
+    private class GetDataTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+            }
+            return "" + (mItemCount++);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mListItems.add(result);
+            mAdapter.notifyDataSetChanged();
+            // Call onRefreshComplete when the list has been refreshed.
+            mPullRefreshListView.onRefreshComplete();
+        }
     }
 }
