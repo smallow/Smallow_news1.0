02763243c600fb4a19ae19ecbd86bf8a39c9f10c
smallow <playdreamist@gmail.com>
2015-05-20 17:14:19 +0800

    2015-05-20 17:15 修改

--- smallow_community/src/main/java/com/smallow/community/adapter/ContentListViewAdapter.java
+++ smallow_community/src/main/java/com/smallow/community/adapter/ContentListViewAdapter.java
@@ -1,8 +1,13 @@
 package com.smallow.community.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.smallow.community.R;
 import com.smallow.community.bean.Content;
 
 /**
@@ -13,7 +18,30 @@ public class ContentListViewAdapter extends InnerBaseAdapter<Content> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView= LayoutInflater.from(parent.getContext()).inflate(R.layout.content_listview_item,null);
+            holder.tvTitle = (TextView) convertView
+                    .findViewById(R.id.tv_title);
+            holder.tvTime = (TextView) convertView
+                    .findViewById(R.id.tv_time);
+            holder.tvContent = (TextView) convertView
+                    .findViewById(R.id.tv_content);
+            convertView.setTag(holder);
+        }
+
         return null;
+    }
+
+
+
+
+    private class ViewHolder {
+        TextView tvTitle;
+        TextView tvMoney;
+        TextView tvDescription;
+        ImageView titleImg;
     }
 }
 
